4db720bffe3b11d8977f53802f104bd43d0cd90d
Jstowers1 <J.stowers1@icloud.com>
2023-10-24 14:20:30 -0500

    Added fade in text

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function navBar() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
+let elementsArray = document.querySelectorAll(".tile");
+console.log(elementsArray);
+window.addEventListener('scroll', fadeIn ); 
+function fadeIn() {
+    for (var i = 0; i < elementsArray.length; i++) {
+        var elem = elementsArray[i]
+        var distInView = elem.getBoundingClientRect().top - window.innerHeight + 20;
+        if (distInView < 0) {
+            elem.classList.add("inView");
+        } else {
+            elem.classList.remove("inView");
+        }
     }
-  } 
+}
+fadeIn();
